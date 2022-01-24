02dcaa450af82c9e9de431b2c7e0669f83d49797
chverma <me@chverma.com>
2022-01-24 23:18:03 +0100

    Remove unused code

--- content_script.js
+++ content_script.js
@@ -6,61 +6,6 @@ chrome.storage.onChanged.addListener((changes, area) => {
         );
     }
     console.log("changes: ", changes, area)
-    if (area === 'local' && 'value' in changes) {
-        if (changes.sepiaLevel.newValue.value != changes.sepiaLevel.oldValue.value)
-            updateSepiaLevel(changes.sepiaLevel.newValue.value);
-    }
 });
-
-function updateSepiaLevel(value) {
-    console.log("updateSepiaLevel: ", value)
-    style.innerText = `html { filter: sepia(${value}%) !important}`;
-}
-
-function setItem() {
-    console.log("OK");
-}
-
-function gotSepiaLevel(item) {
-    console.log(`${Object.keys(item)[0]} has ${item.sepiaLevel}`);
-    updateSepiaLevel(item.sepiaLevel)
-}
-
-function gotCenterPlaces(item, sendResponse) {
-    if (sendResponse)
-        sendResponse(item.centerPlaces);
-    console.log(`${Object.keys(item)[0]} has ${JSON.stringify(item.centerPlaces)}`);
-}
-
-function onError(error) {
-    console.log(error)
-}
-
-
-// For sepia level
-let style = document.createElement('style');
-document.body.appendChild(style);
-
-// define 2 objects
-let sepiaLevel = {
-    "sepiaLevel": 0
-}
-
-let centerPlaces = {
-    "centerPlaces": {}
-}
-
-// store the objects
-chrome.storage.local.set(sepiaLevel)
-    .then(setItem, onError);
-chrome.storage.local.set(centerPlaces)
-    .then(setItem, onError);
-// get the objects
-chrome.storage.local.get("sepiaLevel")
-    .then(gotSepiaLevel, onError);
-chrome.storage.local.get("centerPlaces")
-    .then(gotCenterPlaces, onError);
-
-
 
 console.log("content script done")
