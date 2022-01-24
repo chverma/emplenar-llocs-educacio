b4d8043982e3fff0a053f1a05e9a550536d800c3
chverma <me@chverma.com>
2022-01-24 11:39:21 +0100

    Working extension

--- fillPlaces.js
+++ fillPlaces.js
@@ -1,19 +1,28 @@
 function init() {
-    getAllPlaces(46812)
+    console.log("init1")
+    chrome.storage.local.get('options')
+    .then(res => {
+        var espName = res.options.espName;
+        var espCode = res.options.espCode;
+        var postalCode = res.options.postalCode;
+
+        getAllPlaces(postalCode, espCode, espName)
+    })
+    
 }
 
 // Querying center places from postalCode
-function getAllPlaces(postalCode) {
+function getAllPlaces(postalCode, espCode, espName) {
     let url = `http://decasaalcole.cartodb.com/api/v2/sql?q=with ftimes as (select cp_to cp, from_dist dist, from_time atime from times where cp_from = '${postalCode}'), ttimes as (select cp_from cp, to_dist dist, to_time atime from times where cp_to = '${postalCode}'), totaltimes as (select * from ftimes union select * from ttimes union select '${postalCode}' cp, 0 dist, 0 atime) select c.cartodb_id, c.the_geom, ST_astext(c.the_geom) as tgeom, c.the_geom_webmercator, c.regimen, c.dgenerica, c.dabreviada, c.localidad, c.tipocalle, c.direccion, c.numero, c.codigo, ((t.atime)/60)::integer minutes, (t.dist/1000)::integer kms from coles_cp2 c join totaltimes t on c.cp = t.cp WHERE regimen = true AND ( nived like '%25F%25' or nived like '%25E%25' or nived like '%25D%25' ) order by t.atime, t.dist`
     var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
             if (xmlhttp.status == 200) {
                 rowsResult = JSON.parse(xmlhttp.response);
-                console.log(rowsResult);
+                //console.log(rowsResult);
 
-                fillAllPlaces(rowsResult.rows);
+                fillAllPlaces(rowsResult.rows, espCode, espName);
             } else if (xmlhttp.status == 400) {
                 alert('There was an error 400');
             } else {
@@ -26,31 +35,31 @@ function getAllPlaces(postalCode) {
     xmlhttp.send()
 }
 
-function fillAllPlaces(rows) {
+function fillAllPlaces(rows, espCode, espName) {
     place = {};
-    place.especialitat = "107";
+    place.especialitat = espCode;
     place.centre = '';
     codeAdded = [];
     for (var elem in rows) {
         var code = rows[elem].codigo.toString()
-        var prefix = code.length < 8? '0': ''
+        var prefix = code.length < 8 ? '0' : ''
 
-        place.centre =  prefix + code + 'C';
+        place.centre = prefix + code + 'C';
 
-        if (!codeAdded.includes(place.centre)){
-            fillPlace(place);
+        if (!codeAdded.includes(place.centre)) {
+            fillPlace(place, espName);
             codeAdded.push(place.centre);
         }
     }
 }
-function fillPlace(place) {
-    console.log("fillPlace")
+function fillPlace(place, espName) {
+    //console.log("fillPlace")
     document.querySelector(".afegir").click();
     let formulari_centres = document.querySelector("#formulari_centres");
     let lastPlace = formulari_centres.lastChild;
     let especialitat = lastPlace.querySelector('input[name="especialitat"')
     let centre = lastPlace.querySelector('input[name="centre"')
-        //var jso = document.defaultView.wrappedJSObject; //Access to foreign scripts variables
+    //var jso = document.defaultView.wrappedJSObject; //Access to foreign scripts variables
 
     centre.value = place.centre
     centre.classList.add("CENT")
@@ -61,13 +70,13 @@ function fillPlace(place) {
     centre.parentElement.appendChild(span)
 
     especialitat.value = place.especialitat
-    especialitat.parentElement.lastChild.innerHTML = "INFORMÀTICA"
+    especialitat.parentElement.lastChild.innerHTML = espName
 }
 
 function verificarCentro(cod, span) {
     var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState == XMLHttpRequest.DONE) { // XMLHttpRequest.DONE == 4
             if (xmlhttp.status == 200) {
                 //console.log(xmlhttp.responseText);
@@ -83,7 +92,7 @@ function verificarCentro(cod, span) {
             }
         }
     };
-    console.log("verificarCentro")
+    //console.log("verificarCentro")
     vars = "codi=" + cod;
 
     var formData = new FormData();
--- popup.js
+++ popup.js
@@ -1,76 +1,26 @@
-let sepiaLevel = -1;
-let centerPlaces = {};
+function checkRequiredInput() {
+    var espCode = document.querySelector('#especialitatCode').value;
+    var espName = document.querySelector('#especialitatName').value;
+    var postalCode = document.querySelector('#postalCode').value;
 
+    if (espName && espCode && postalCode) {
+        var options = {
+            'options': {
+                'espName': espName,
+                'espCode': espCode,
+                'postalCode': postalCode
+            }
+        };
 
+        chrome.storage.local.set(options)
+        setItem();
 
-
-chrome.runtime.sendMessage('get-user-data', (response) => {
-    // 3. Got an asynchronous response with the data from the background
-    console.log('received user data', response);
-
-});
-
-function gotSepiaLevel(item) {
-    console.log(`${Object.keys(item)[0]} has ${item.sepiaLevel}`);
-}
-
-function gotCenterPlaces(item) {
-    console.log(`${Object.keys(item)[0]} has ${JSON.stringify(item.centerPlaces)}`);
-}
-
-
-function onError(error) {
-    console.log("Ta tia")
-    console.log(error)
-}
-
-// initialize
-async function init() {
-
-    let input = document.querySelector('#sepia_range');
-    input.addEventListener('change', e => {
-        //e.target.value
-    });
-    chrome.runtime.sendMessage({
-        request: "centerPlaces" //get from tab's local storage
-    });
-    console.log("init1");
-    // get the objects
-    console.log("localStorage: ", localStorage)
-    chrome.storage.local.get("sepiaLevel")
-        .then(gotSepiaLevel, onError);
-    console.log("init2");
-    chrome.storage.local.get("centerPlaces")
-        .then(gotCenterPlaces, onError);
-
-
-
-    /*if (!sepiaLevel) {
-        sepiaLevel = {};
-        sepiaLevel.name = 'sepiaLevel'
-        sepiaLevel.value = 0
+    } else {
+        return false;
     }
-    if (!centerPlaces) {
-        centerPlaces = {};
-        centerPlaces.name = 'centerPlaces'
-        centerPlaces.value = {}
-    }
-
-    input.value = sepiaLevel.value;*/
-
-    //chrome.storage.local.set({ sepiaLevel, centerPlaces });
-    //console.log("init__", browser.storage.local.get('sepiaLevel').item)
 }
 
-
-// Run button on click
-document.querySelector('#run_btn').addEventListener('click', () => {
-    console.error("CLICKEDDDDDD")
-    chrome.runtime.sendMessage('get-all-places', (response) => {
-        // 3. Got an asynchronous response with the data from the background
-        console.log('received user data', response);
-
-    });
+function setItem() {
     let tabID = -1;
     // Querying browser tab
     let querying = browser.tabs.query({ currentWindow: true, active: true });
@@ -97,7 +47,11 @@ document.querySelector('#run_btn').addEventListener('click', () => {
     function onExecuted(result) {
         console.log(`onExecuted`);
     }
+}
+// Run button on click
+document.querySelector('#run_btn').addEventListener('click', () => {
+    return checkRequiredInput()
+
 });
 
-//init(); //.catch(e => console.error(e));
 console.log("popup.js done");
